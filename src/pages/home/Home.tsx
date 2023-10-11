5538128a00cd41bcf7152ce3da1194c79a8c489f
Camilo Vargas <camilovargasingm@gmail.com>
2023-10-11 15:34:51 -0500

    feat: implement basic component in table list

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,17 @@
-import Header from "../../components/Header";
-
+import Header from "@/components/Header";
+import { Button } from "@/components/ui/Button";
+import { Input } from "@/components/ui/Input";
 
 export default function Home() {
   return (
     <>
       <Header />
       <main className="flex justify-center mt-8">
-        <div className="max-w-3xl bg-primary text-primary-foreground rounded-lg p-8">
-          Table list
+        <div className="bg-primary text-primary-foreground rounded-lg p-8 min-w-[30rem]">
+          <header className="flex justify-between">
+            <Input type="text" className="rounded-e-none border-e-0 w-full" />
+            <Button className="rounded-s-none" variant={"secondary"} >Add Task</Button>
+          </header>
         </div>
       </main>
     </>
