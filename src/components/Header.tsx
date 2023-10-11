b9a42a992033aec1632a2296ec79908342b8f71c
Camilo Vargas <camilovargasingm@gmail.com>
2023-10-11 16:35:26 -0500

    fix: colors and style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 
 export default function Header() {
   return (
-    <header className="w-full p-5 text-center font-semibold text-2xl">CS3 · Technical test · To-Do list</header>
+    <header className="w-full p-5 text-center font-semibold text-2xl bg-primary">CS3 · Technical Test · To-Do list</header>
   )
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,19 @@
 import Header from "@/components/Header";
 import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/Input";
+import ToDoList from "./components/ToDoList";
 
 export default function Home() {
   return (
     <>
       <Header />
       <main className="flex justify-center mt-8">
-        <div className="bg-primary text-primary-foreground rounded-lg p-8 min-w-[30rem]">
+        <div className="flex flex-col gap-4 bg-primary text-primary-foreground rounded-lg p-8 w-[30rem]">
           <header className="flex justify-between">
-            <Input type="text" className="rounded-e-none border-e-0 w-full" />
-            <Button className="rounded-s-none" variant={"secondary"} >Add Task</Button>
+            <Input type="text" className="rounded-e-none border-e-0 w-full h-10" placeholder="What do you need to do?" />
+            <Button className="rounded-s-none h-10" variant={"secondary"} aria-label="add task">Add Task</Button>
           </header>
+          <ToDoList />
         </div>
       </main>
     </>
