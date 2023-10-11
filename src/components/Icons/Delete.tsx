ed521e856dd5dc09d2eafbbc77229ec9eaee0273
Camilo Vargas <camilovargasingm@gmail.com>
2023-10-11 16:12:13 -0500

    feat: add icons

---
+++ src/components/Icons/Delete.tsx
@@ -0,0 +1,5 @@
+import { Trash2, type LucideProps } from 'lucide-react'
+
+export function IconDelete(props: LucideProps) {
+  return <Trash2 {...props} />
+}
---
+++ src/components/Icons/Edit.tsx
@@ -0,0 +1,5 @@
+import { ClipboardEdit, type LucideProps } from 'lucide-react'
+
+export function IconEdit(props: LucideProps) {
+  return <ClipboardEdit {...props} />
+}
