1cfc3d81615427e93d8122550d25478a6347b21c
Camilo Vargas <camilovargasingm@gmail.com>
2023-10-11 16:13:22 -0500

    feat: create basic list of task

---
+++ src/pages/home/components/ToDoList.tsx
@@ -0,0 +1,31 @@
+import { IconDelete, IconEdit } from "@/components/Icons"
+import { Button } from "@/components/ui/Button"
+
+const MockToDoList = [
+  { id: 1, title: 'Task 1' },
+  { id: 2, title: 'Prepare a dish from a foreign culture Task 2 with more text for rebase' },
+  { id: 3, title: 'Prepare a dish from a foreign culture' },
+]
+
+export default function ToDoList() {
+  return (
+    <ul className="flex flex-col gap-2">
+      {MockToDoList.map((task) => (
+        <li
+          className="bg-secondary text-secondary-foreground rounded-md p-2 pr-0 flex gap-2 justify-between items-center"
+          key={task.id}
+        >
+          <div className="truncate" title={task.title}>{task.title}</div>
+          <div className="flex">
+            <Button variant={"ghost"} className="hover:bg-transparent hover:text-primary" size={"icon"} aria-label="edit task">
+              <IconEdit className='h-5 w-5' />
+            </Button>
+            <Button variant={"ghost"} className="hover:bg-transparent hover:text-primary" size={"icon"} aria-label="delete task">
+              <IconDelete className='h-5 w-5' />
+            </Button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
+}
