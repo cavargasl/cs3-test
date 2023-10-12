50c0513e130d56e935c6f162b5b5b4b7797d4534
Camilo Vargas <camilovargasingm@gmail.com>
2023-10-11 23:02:09 -0500

    feat: implement reactQuery and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { TailwindIndicator } from "./components/TailwindIndicator"
 import ThemeToggle from "./components/ThemeToggle"
 import { Home } from "./pages/home"
+import './config/axios.config'
+
+const queryClient = new QueryClient()
 
 function App() {
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Home />
       <ThemeToggle />
       <TailwindIndicator />
-    </>
+    </QueryClientProvider>
   )
 }
 
