3832768777827e6c2c687673b67bf61cc17d916b
Camilo Vargas <camilovargasingm@gmail.com>
2023-10-12 17:29:37 -0500

    feat: add icon

---
+++ src/components/Icons/CheckBox.tsx
@@ -0,0 +1,5 @@
+import { CheckCircle2, type LucideProps } from 'lucide-react'
+
+export function IconCheckBox(props: LucideProps) {
+  return <CheckCircle2 {...props} />
+}
--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,3 +1,4 @@
+export * from './CheckBox';
 export * from './Close';
 export * from './Delete';
 export * from './Edit';
