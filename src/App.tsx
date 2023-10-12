82ca98dc1aa23175befaf96ccd12821594bb3916
Camilo Vargas <camilovargasingm@gmail.com>
2023-10-11 22:26:18 -0500

    feat: create tailwind indicator

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { TailwindIndicator } from "./components/TailwindIndicator"
 import { Home } from "./pages/home"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Home />
+      <TailwindIndicator />
     </>
   )
 }
