bdcc7c3d86e523098bf559c240fd7e1485b97cba
Camilo Vargas <camilovargasingm@gmail.com>
2023-10-11 23:42:57 -0500

    feat: implement redux in the todo list component

--- src/pages/home/components/ToDoList/ToDoList.tsx
+++ src/pages/home/components/ToDoList/ToDoList.tsx
@@ -2,16 +2,23 @@ import { IconDelete, IconEdit } from "@/components/Icons"
 import { Button } from "@/components/ui/Button"
 import { useQuery } from "@tanstack/react-query"
 import { getTodos } from "./services"
+import { useDispatch, useSelector } from "react-redux";
+import { addListTodos, selectTodos } from "@/redux/slice/todos";
 
 export default function ToDoList() {
+  const dispatch = useDispatch();
+  const todosList = useSelector(selectTodos);
   const { data, isLoading, isError } = useQuery({ queryKey: ['todos'], queryFn: getTodos })
-  
+
   if (isLoading) return <p className="text-center font-medium text-xl">Loading...</p>
   if (isError) return <p className="text-center font-medium text-xl text-red-500">{"Ops... something has gone wrong"}</p>
   if (!data) return <p className="text-center font-medium text-xl">List of todos is empty</p>
+  if (data) {
+    dispatch(addListTodos(data));
+  }
   return (
     <ul className="flex flex-col gap-2" data-testid="to-do-list">
-      {data.todos.map((task) => (
+      {todosList.todos.map((task) => (
         <li
           className="bg-secondary text-secondary-foreground rounded-md p-2 pr-0 flex gap-2 justify-between items-center"
           key={task.id}
