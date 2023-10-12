28cd7a5c2c3a5fe4f053d3a51313fbac0a7eedb8
Camilo Vargas <camilovargasingm@gmail.com>
2023-10-12 14:08:49 -0500

    feat: add loader icon

---
+++ src/components/Icons/Loader.tsx
@@ -0,0 +1,5 @@
+import { Loader2, type LucideProps } from 'lucide-react'
+
+export function IconLoader(props: LucideProps) {
+  return <Loader2 {...props} />
+}
--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,4 +1,5 @@
 export * from './Delete';
 export * from './Edit';
+export * from './Loader';
 export * from './Moon';
 export * from './Sun';
