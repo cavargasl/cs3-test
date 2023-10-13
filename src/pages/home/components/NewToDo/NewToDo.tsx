7fddb424ed98b64d26c8888ebe02aabd8feb5a44
Camilo Vargas <camilovargasingm@gmail.com>
2023-10-13 11:59:40 -0500

    fix: unique id and send filter

--- src/pages/home/components/NewToDo/NewToDo.tsx
+++ src/pages/home/components/NewToDo/NewToDo.tsx
@@ -1,15 +1,18 @@
+import { CheckNewTodo } from '@/lib/validations/todo'
+import { selectTodos } from '@/redux/slice/todos'
 import { ToDoList } from '@/types'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useForm } from 'react-hook-form'
+import { useSelector } from 'react-redux'
 import { z } from 'zod'
 import { InputToDo } from '../InputToDo'
 import { newTodoService } from './services'
-import { CheckNewTodo } from '@/lib/validations/todo'
 
 export type InputTodo = z.infer<typeof CheckNewTodo>
 
 export default function NewToDo() {
+  const { filter } = useSelector(selectTodos)
   const { register, handleSubmit, formState: { errors }, reset } = useForm<InputTodo>({
     resolver: zodResolver(CheckNewTodo),
     defaultValues: {
@@ -25,8 +28,17 @@ export default function NewToDo() {
     mutate({ completed: false, userId: 5, todo: data.title }, {
       onSuccess: (response) => {
         reset()
-        queryClient.setQueryData(['todos'], (old?: ToDoList) => {
-          return { todos: [response, ...old?.todos.slice(0, 4) || []], limit: old?.limit || 5, total: old?.total ? old.total + 1 : 0, skip: old?.skip || 0 }
+        queryClient.setQueryData(['todos'], (old?: Omit<ToDoList, "filter">) => {
+          if (old?.todos.find((item) => item.id === response.id)) {
+            response.id++
+          }
+          return {
+            todos: [response, ...(old?.todos?.slice(0, 4) || [])],
+            limit: old?.limit || 5,
+            total: old?.total ? old.total + 1 : 0,
+            skip: old?.skip || 0,
+            filter: { ...filter }
+          };
         })
       }
     })
